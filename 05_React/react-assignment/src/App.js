7cb3d6631c8cabb4d449111943156811b008c5d8
cozekouzi <omarkouzi81248@gmail.com>
2022-12-20 12:28:42 +0000

    made part of the header

--- 05_React/react-assignment/src/App.js
+++ 05_React/react-assignment/src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 function App() {
   return (
     <>  
-    <header className="header"></header>
+    <header className="header">
+      <h2 className="london">london</h2>
+      <button className="find-Weather-btn">FIND WEATHER</button>
+    </header>
     <body className="App-body">
-<p>hello</p>
+<div className="main">
+<div className="daily-weather">
+  <div className="image"></div>
+<div><p>overcast clouds</p></div>
+</div>
+<div className="weekly-weather"></div>
+
+</div>
 
 
 
