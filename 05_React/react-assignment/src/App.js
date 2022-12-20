1922734f945e8209eb8ef843997e18ceb177f9dc
cozekouzi <omarkouzi81248@gmail.com>
2022-12-20 11:35:22 +0000

    first commit

---
+++ 05_React/react-assignment/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <>  
+    <header className="header"></header>
+    <body className="App-body">
+<p>hello</p>
+
+
+
+
+    </body>
+    </>
+  );
+}
+
+export default App;
